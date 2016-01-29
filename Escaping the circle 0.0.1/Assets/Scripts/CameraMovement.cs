cda23e3c577fd01f3b287722368d0deb2165968d
Alexanna Eike Cayeux <alexanna.ec.95@gmail.com>
2016-01-29 21:53:43 +0100

    Added Unity project

---
+++ Escaping the circle 0.0.1/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Vector3[] _prevMousePos = new Vector3[12];
+    private int _index = 0;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _prevMousePos[_index] = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(_index >= _prevMousePos.Length)
+        { _index = 0; }else { _index++; }
+
+
+	}
+}
