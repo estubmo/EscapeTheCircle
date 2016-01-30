25fca63fe44f90ba582e04e2210d3687f01ed29f
Alexanna Eike Cayeux <alexanna.ec.95@gmail.com>
2016-01-30 10:14:12 +0100

    Added smoothening to camera pan, started working on flaw finding

--- Escaping the circle 0.0.1/Assets/Scripts/CameraMovement.cs
+++ Escaping the circle 0.0.1/Assets/Scripts/CameraMovement.cs
@@ -1,41 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CameraMovement : MonoBehaviour
 {
     private Transform _tr;
+    private Camera _camera;
 
+    private List<GameObject> _flawGO = new List<GameObject>();
     private Vector3[] _prevMousePos = new Vector3[12];
     private int _index = 0;
     private Vector3[] _prevMousePosViewPort = new Vector3[12];
+    private Ray _mouseToWorldRay;
+    private RaycastHit _mouseRayHit;
+
     public float _sensitivityOfViewPort;
     public float _SpeedOfRotation;
+    public float _flawErrorMergin;
+
     // Use this for initialization
     void Start ()
     {
         _tr = GetComponent<Transform>();
-	}
+        _camera = Camera.main;
+        _flawGO.AddRange(GameObject.FindGameObjectsWithTag("Flaw"));
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //Loading array for both smoothening purpus, and to check if mouse/eyes are forcused on one spot
         if (_index >= _prevMousePos.Length-1)
         { _index = 0; }
         else { _index++; }
-        _prevMousePos[_index] = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        _prevMousePosViewPort[_index] = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        _prevMousePos[_index] = _camera.ScreenToWorldPoint(Input.mousePosition);
+        _prevMousePosViewPort[_index] = _camera.ScreenToViewportPoint(Input.mousePosition);
+        _mouseToWorldRay = _camera.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(_mouseToWorldRay, out _mouseRayHit, 100f);
+        Debug.DrawRay(_mouseToWorldRay.origin, _mouseToWorldRay.direction, Color.red);
 
-        if (_prevMousePosViewPort[_index].x > 1f- _sensitivityOfViewPort) //RightSide
+        //Debug.Log(_mouseRayHit.collider.name);
+
+        ViewPortMovement();
+
+        FindFlaw();
+
+    }
+
+    #region FindFlaw
+    void FindFlaw()
+    {
+        if (_mouseRayHit.collider.tag == "Flaw")
         {
-            _tr.Rotate(Vector3.up * Time.deltaTime * _SpeedOfRotation, Space.World);
+            Debug.Log("FoundFlaw");
+        }
+    }
+    #endregion
+
+    #region ViewPort Movement
+    void ViewPortMovement()
+    {
+        if (_prevMousePosViewPort[_index].x > 1f - _sensitivityOfViewPort) //RightSide
+        {
+            _tr.Rotate(Vector3.up * Time.deltaTime * _SpeedOfRotation * ((_prevMousePosViewPort[_index].x - (1f - _sensitivityOfViewPort)) / (_sensitivityOfViewPort)), Space.World);
             Debug.Log("GoRight");
         }
         if (_prevMousePosViewPort[_index].x < _sensitivityOfViewPort) //LeftSide
         {
-            _tr.Rotate(Vector3.up * Time.deltaTime * _SpeedOfRotation * -1, Space.World);
+            _tr.Rotate(Vector3.up * Time.deltaTime * _SpeedOfRotation * -1 * ((_sensitivityOfViewPort - _prevMousePosViewPort[_index].x) / _sensitivityOfViewPort), Space.World);
             Debug.Log("GoLeft");
         }
-        if (_prevMousePosViewPort[_index].y > 1f- _sensitivityOfViewPort) //Up
+        if (_prevMousePosViewPort[_index].y > 1f - _sensitivityOfViewPort) //Up
         {
             Debug.Log("GoUp");
         }
@@ -44,4 +79,5 @@ public class CameraMovement : MonoBehaviour
             Debug.Log("GoDown");
         }
     }
+    #endregion
 }
