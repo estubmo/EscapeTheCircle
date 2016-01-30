93981039ccc04b797891e71f11f399855c4ba7e0
tromat14 <trmat14@student.westerdals.no>
2016-01-30 05:22:46 +0100

    Startet work on level 1
    MAde changes in CameraMovement Script sp that it now rotates sideways

--- Escaping the circle 0.0.1/Assets/Scripts/CameraMovement.cs
+++ Escaping the circle 0.0.1/Assets/Scripts/CameraMovement.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour
 {
+    private Transform _tr;
+
     private Vector3[] _prevMousePos = new Vector3[12];
     private int _index = 0;
     private Vector3[] _prevMousePosViewPort = new Vector3[12];
     public float _sensitivityOfViewPort;
+    public float _SpeedOfRotation;
     // Use this for initialization
     void Start ()
     {
-	
+        _tr = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
@@ -24,10 +27,12 @@ public class CameraMovement : MonoBehaviour
 
         if (_prevMousePosViewPort[_index].x > 1f- _sensitivityOfViewPort) //RightSide
         {
+            _tr.Rotate(Vector3.up * Time.deltaTime * _SpeedOfRotation, Space.World);
             Debug.Log("GoRight");
         }
         if (_prevMousePosViewPort[_index].x < _sensitivityOfViewPort) //LeftSide
         {
+            _tr.Rotate(Vector3.up * Time.deltaTime * _SpeedOfRotation * -1, Space.World);
             Debug.Log("GoLeft");
         }
         if (_prevMousePosViewPort[_index].y > 1f- _sensitivityOfViewPort) //Up
