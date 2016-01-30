ab7de441bf15e4780002e6797fdccb74f9e0b3a1
Alexanna Eike Cayeux <alexanna.ec.95@gmail.com>
2016-01-30 19:16:20 +0100

    small Cleanup

--- Escaping the circle 0.0.1/Assets/Scripts/CameraMovement.cs
+++ Escaping the circle 0.0.1/Assets/Scripts/CameraMovement.cs
@@ -18,14 +18,14 @@ public class CameraMovement : MonoBehaviour
     private Ray _mouseToWorldRay;
     private RaycastHit _mouseRayHit;
 
-	private float mouseTimer = 2.5f;
-	private const float mouseTimerLimit = 3.0f;
-	private bool mouseActive;
+    private float mouseTimer = 2.5f;
+    private const float mouseTimerLimit = 3.0f;
+    private bool mouseActive;
 
-	private GazePointDataComponent gaze;
-	private EyeXHost _eyexHost;
+    private GazePointDataComponent gaze;
+    private EyeXHost _eyexHost;
 
-	private Vector3 inputPosition;
+    private Vector3 inputPosition;
 
     private string _flawName;
     private string _prevName;
@@ -40,28 +40,28 @@ public class CameraMovement : MonoBehaviour
     public float _sensitivityOfViewPort;
     public float _SpeedOfRotation;
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         _tr = GetComponent<Transform>();
         _camera = Camera.main;
         _flawGO.AddRange(GameObject.FindGameObjectsWithTag("Flaw"));
-		gaze = GetComponent <GazePointDataComponent> ();
-		_eyexHost = EyeXHost.GetInstance ();
+        gaze = GetComponent<GazePointDataComponent>();
+        _eyexHost = EyeXHost.GetInstance();
         _handAnim = gameObject.GetComponentInChildren<Animator>();
         _handTrans = _tr.GetChild(0);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         //----------Loading array for both smoothening purpus, and to check if mouse/eyes are forcused on one spot
-        if (_index >= _prevMousePos.Length-1)
+        if (_index >= _prevMousePos.Length - 1)
         { _index = 0; }
         else { _index++; }
-		isMouseActive ();
-		_prevMousePos[_index] = _camera.ScreenToWorldPoint(getInputPosition ());
-		_prevMousePosViewPort[_index] = _camera.ScreenToViewportPoint(getInputPosition ());
-		_mouseToWorldRay = _camera.ScreenPointToRay(getInputPosition ());
+        isMouseActive();
+        _prevMousePos[_index] = _camera.ScreenToWorldPoint(getInputPosition());
+        _prevMousePosViewPort[_index] = _camera.ScreenToViewportPoint(getInputPosition());
+        _mouseToWorldRay = _camera.ScreenPointToRay(getInputPosition());
         Physics.Raycast(_mouseToWorldRay, out _mouseRayHit, 100f);
         Debug.DrawRay(_mouseToWorldRay.origin, _mouseToWorldRay.direction, Color.red);
 
@@ -72,7 +72,7 @@ public class CameraMovement : MonoBehaviour
         FindFlaw();
 
         HandAnimation();
-        
+
 
         //----------Load next level when no Flaws left
         if (_flawGO.Count == 0)
@@ -95,7 +95,7 @@ public class CameraMovement : MonoBehaviour
 
         //Debug.Log(_averagePos.ToString() + " : " + _prevMousePosViewPort[_index].ToString());
         _timeAnim -= Time.deltaTime;
-        
+
 
         if (_handAnim.GetBool("ReachOut") && _timeAnim <= 0)
         { _handAnim.SetBool("ReachOut", false); }
@@ -111,7 +111,7 @@ public class CameraMovement : MonoBehaviour
         if (_timeAnimStart < 0)
         { _timeAnimStart = 0; }
 
-        if(_timeAnimStart > 0.5f)
+        if (_timeAnimStart > 0.5f)
         {
             _handAnim.SetBool("ReachOut", true);
             _prevAveragePos = _averagePos;
@@ -129,7 +129,7 @@ public class CameraMovement : MonoBehaviour
             );
         }
         else
-        { 
+        {
             _ang = Angle
                 (
                     _prevMousePosViewPort[_index].x - 0.5f,
@@ -138,64 +138,82 @@ public class CameraMovement : MonoBehaviour
                 );
         }
         if (float.IsNaN(_ang)) { _ang = 90f; }
-        _ang += (_prevMousePosViewPort[_index].x - 0.5f)*0.8f;
+        _ang += (_prevMousePosViewPort[_index].x - 0.5f) * 0.8f;
         _handTrans.localEulerAngles = new Vector3(31, 0, -_ang * Mathf.Rad2Deg - 90);
     }
     #endregion
 
     #region InputHandling
-    public void isMouseActive(){
-		var eyeTrackerDeviceStatus = _eyexHost.EyeTrackingDeviceStatus;
-		if (eyeTrackerDeviceStatus == EyeXDeviceStatus.Tracking) {
-			if (Input.GetAxis ("Mouse X") != 0 || Input.GetAxis ("Mouse Y") != 0) {
-				mouseTimer = 0;
-			}
-			mouseTimer += Time.deltaTime;
-			if (mouseTimer >= mouseTimerLimit) {
-				mouseActive = false;
-			} else {
-				mouseActive = true;
-			}
-		} else {
-			mouseActive = true;
-			mouseTimer = 0;
-		}
+    public void isMouseActive()
+    {
+        var eyeTrackerDeviceStatus = _eyexHost.EyeTrackingDeviceStatus;
+        if (eyeTrackerDeviceStatus == EyeXDeviceStatus.Tracking)
+        {
+            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+                mouseTimer = 0;
+            }
+            mouseTimer += Time.deltaTime;
+            if (mouseTimer >= mouseTimerLimit)
+            {
+                mouseActive = false;
+            }
+            else {
+                mouseActive = true;
+            }
+        }
+        else {
+            mouseActive = true;
+            mouseTimer = 0;
+        }
 
-		//Debug.Log ("mouseTimer: " + mouseTimer + " mouseActive: " + mouseActive); 
-	}
+        //Debug.Log ("mouseTimer: " + mouseTimer + " mouseActive: " + mouseActive); 
+    }
 
-	public Vector3 getInputPosition(){
-		if (mouseActive) {
-			return Input.mousePosition;
-		} else {
-			return new Vector3(gaze.LastGazePoint.Screen.x,gaze.LastGazePoint.Screen.y,0);
-		}
-	}
+    public Vector3 getInputPosition()
+    {
+        if (mouseActive)
+        {
+            return Input.mousePosition;
+        }
+        else {
+            return new Vector3(gaze.LastGazePoint.Screen.x, gaze.LastGazePoint.Screen.y, 0);
+        }
+    }
 
-	#endregion
+    #endregion
 
     #region FindFlaw
     void FindFlaw()
     {
-		if (_mouseRayHit.collider != null) {
-			if (_mouseRayHit.collider.tag == "Flaw") {
-				if (_mouseRayHit.collider.name != _flawName || _flawTimer > _timeNeededToFindFlaw) {
-					_flawName = _mouseRayHit.collider.name;
-					_flawTimer = _timeNeededToFindFlaw;
-				}
-			}
-			if (_flawName == _mouseRayHit.collider.name) {
-				_flawTimer -= Time.deltaTime;
-			} else {
-				_flawTimer += Time.deltaTime;
-			}
-			if (_flawTimer <= 0f) {
-				//Debug.Log ("Found Flaw");
-				_flawGO.Remove (_mouseRayHit.collider.gameObject);
-				Destroy (_mouseRayHit.collider.gameObject);
-			}
-		}
-	}
+        if (!_isMoving)
+        {
+            if (_mouseRayHit.collider != null)
+            {
+                if (_mouseRayHit.collider.tag == "Flaw")
+                {
+                    if (_mouseRayHit.collider.name != _flawName || _flawTimer > _timeNeededToFindFlaw)
+                    {
+                        _flawName = _mouseRayHit.collider.name;
+                        _flawTimer = _timeNeededToFindFlaw;
+                    }
+                }
+                if (_flawName == _mouseRayHit.collider.name)
+                {
+                    _flawTimer -= Time.deltaTime;
+                }
+                else {
+                    _flawTimer += Time.deltaTime;
+                }
+                if (_flawTimer <= 0f)
+                {
+                    //Debug.Log ("Found Flaw");
+                    _flawGO.Remove(_mouseRayHit.collider.gameObject);
+                    Destroy(_mouseRayHit.collider.gameObject);
+                }
+            }
+        }
+    }
     #endregion
 
     #region ViewPort Movement
