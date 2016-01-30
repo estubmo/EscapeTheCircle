bd94501b970928cb6cd1dd15c8a022470e142b83
tromat14 <trmat14@student.westerdals.no>
2016-01-30 03:09:34 +0100

    Adden main menu and credits + button actionScript to move between scenes

---
+++ Escaping the circle 0.0.1/Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+//This script is a collection different actions typical for a button
+// Mattias Tronslien 2016
+
+public class ButtonActions : MonoBehaviour {
+
+    public void ChangeLevel(int IndexOfScene) {
+        SceneManager.LoadScene(IndexOfScene);
+    } 
+
+
+}
