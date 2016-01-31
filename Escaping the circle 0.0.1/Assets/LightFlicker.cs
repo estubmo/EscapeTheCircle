3ead627c6735915a63d037925a29234c0607c79c
tromat14 <trmat14@student.westerdals.no>
2016-01-31 12:41:53 +0100

    added mood lighting på level 2

---
+++ Escaping the circle 0.0.1/Assets/LightFlicker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+    private Light _lt;
+    public float _flickerStrength;
+    private float _baseIntencisty;
+
+	// Use this for initialization
+	void Start () {
+        _lt = GetComponent<Light>();
+        _baseIntencisty = _lt.intensity;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _lt.intensity = Random.Range(_baseIntencisty -_flickerStrength, _baseIntencisty + _flickerStrength);
+	}
+}
