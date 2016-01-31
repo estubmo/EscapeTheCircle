e97c4d4fd47eb004d652900f8720fefc955500e3
Dauntless- <eirik_stub@hotmail.com>
2016-01-31 13:50:04 +0100

    Gaze Ãpå krittboks og kniv

---
+++ Escaping the circle 0.0.1/Assets/Scripts/RotateShakeOnGaze.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Script for a game object that grows dynamically when it senses the user's gaze.
+/// </summary>
+[RequireComponent(typeof(GazeAwareComponent))]
+public class RotateShakeOnGaze : MonoBehaviour
+{
+	private static readonly Vector3 NormalScale = new Vector3(0.3f, 0.3f, 0.3f);
+	private static readonly Vector3 LargeScale = new Vector3(0.6f, 0.6f, 0f);
+	private Vector3 origPos;
+
+	private float _scaleFactor = 0;
+	private float timeToGazeBeforeShake;
+	private float timer;
+	private bool transformDirectionToggle;
+
+	private GazeAwareComponent _gazeAwareComponent;
+
+	protected void Start()
+	{
+		_gazeAwareComponent = GetComponent<GazeAwareComponent>();
+		origPos = transform.position;
+
+	}
+
+	/// <summary>
+	/// Update interactor bounds and transform
+	/// </summary>
+	protected void Update()
+	{
+		// Update the scale factor depending on whether the eye-gaze is on the object or not.
+		if (_gazeAwareComponent.HasGaze)
+		{
+			timeToGazeBeforeShake += Time.deltaTime;
+
+			if (timeToGazeBeforeShake > 1.0f) {
+				timer += Time.deltaTime;
+				if (timer >= 0.04f) {
+					if (!transformDirectionToggle) {
+						transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y + 5.0f, transform.eulerAngles.z);
+						transformDirectionToggle = true;
+					} else {
+						transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y - 5.0f, transform.eulerAngles.z);
+						transformDirectionToggle = false;
+					}
+					timer = 0.0f;
+				}
+			}
+		}
+		else
+		{
+			//_scaleFactor = Mathf.Clamp01(_scaleFactor - speed * Time.deltaTime);
+			timeToGazeBeforeShake = 0.0f;
+		}
+		//transform.localScale = Vector3.Slerp(NormalScale, LargeScale, _scaleFactor);
+	}
+}
--- Escaping the circle 0.0.1/Assets/Scripts/ShakeOnGaze.cs
+++ Escaping the circle 0.0.1/Assets/Scripts/ShakeOnGaze.cs
@@ -4,21 +4,24 @@
 /// Script for a game object that grows dynamically when it senses the user's gaze.
 /// </summary>
 [RequireComponent(typeof(GazeAwareComponent))]
-public class GrowOnGaze : MonoBehaviour
+public class ShakeOnGaze : MonoBehaviour
 {
 	private static readonly Vector3 NormalScale = new Vector3(0.3f, 0.3f, 0.3f);
 	private static readonly Vector3 LargeScale = new Vector3(0.6f, 0.6f, 0f);
 	private Vector3 origPos;
 
 	private float _scaleFactor = 0;
+	private float timeToGazeBeforeShake;
+	private float timer;
+	private bool transformDirectionToggle;
 
-	public float speed = 10.0f;
 	private GazeAwareComponent _gazeAwareComponent;
 
 	protected void Start()
 	{
 		_gazeAwareComponent = GetComponent<GazeAwareComponent>();
 		origPos = transform.position;
+
 	}
 
 	/// <summary>
@@ -29,15 +32,27 @@ public class GrowOnGaze : MonoBehaviour
 		// Update the scale factor depending on whether the eye-gaze is on the object or not.
 		if (_gazeAwareComponent.HasGaze)
 		{
-			//transform.position = transform.position.x + 1;
-			//transform.Rotate(Vector3.down);
-			//_scaleFactor = Mathf.Clamp01(_scaleFactor + speed * Time.deltaTime);
+			timeToGazeBeforeShake += Time.deltaTime;
+
+			if (timeToGazeBeforeShake > 1.0f) {
+				timer += Time.deltaTime;
+				if (timer >= 0.04f) {
+					if (!transformDirectionToggle) {
+						transform.position = new Vector3 (transform.position.x + 0.01f, transform.position.y + 0.01f, transform.position.z + 0.01f);
+						transformDirectionToggle = true;
+					} else {
+						transform.position = new Vector3 (transform.position.x - 0.01f, transform.position.y - 0.01f, transform.position.z - 0.01f);
+						transformDirectionToggle = false;
+					}
+					timer = 0.0f;
+				}
+			}
 		}
-			else
+		else
 		{
 			//_scaleFactor = Mathf.Clamp01(_scaleFactor - speed * Time.deltaTime);
-			transform.localPosition = origPos;
+			timeToGazeBeforeShake = 0.0f;
 		}
-		transform.localScale = Vector3.Slerp(NormalScale, LargeScale, _scaleFactor);
+		//transform.localScale = Vector3.Slerp(NormalScale, LargeScale, _scaleFactor);
 	}
 }
